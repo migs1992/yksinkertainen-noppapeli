b9ef3abf8f07818a4b27ff3565b0da82a9da0504
migs1992 <45798946+migs1992@users.noreply.github.com>
2023-05-09 02:50:29 +0300

    Add files via upload

---
+++ tehtävä4.js
@@ -0,0 +1,17 @@
+(returnNums (10) {
+	var Nums = <6;
+    var maxScore = 50;
+    var rolls = 10;
+	 if (roll1 === 6) {
+                console.log("Minimi");
+                return true;
+            } else {
+                console.log("maksimi");
+            } else {
+                console.log("keskiarvo")
+            }
+        }
+    } while (roll1 === roll2);
+    return false;
+}
+}());
